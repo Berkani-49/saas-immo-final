9a1c03bc81b973ab6caa410dce97fc282e38701b
Berkani490000 <amir.elattaoui@student.uclouvain.be>
2025-11-24 03:47:02 +0100

    Fix: enlever les restriction

--- saas-immo-frontend/src/main.jsx
+++ saas-immo-frontend/src/main.jsx
@@ -1,18 +1,18 @@
+// Fichier : src/main.jsx
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import 'react-big-calendar/lib/css/react-big-calendar.css';
+import 'react-big-calendar/lib/css/react-big-calendar.css'; 
 import { BrowserRouter } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
-import theme from './theme.js'; // Importe le thème
+import theme from './theme.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <ChakraProvider theme={theme}>
-        <App />
-      </ChakraProvider>
-    </BrowserRouter>
-  </React.StrictMode>,
+  <BrowserRouter>
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
+  </BrowserRouter>
 );
